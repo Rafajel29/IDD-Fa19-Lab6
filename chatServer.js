7f95be92c5a2aa01ea3f041e701beb9891babbdc
Rafajel29 <fjfourie29@gmail.com>
2019-10-18 10:55:54 -0700

    Completed Lab 6 rev 1

--- chatServer.js
+++ chatServer.js
@@ -9,6 +9,8 @@ var app = express(); // webapp
 var http = require('http').Server(app); // connects http library to server
 var io = require('socket.io')(http); // connect websocket library to server
 var serverPort = 8000;
+var days = 0;
+var tdays = 0;
 
 
 //---------------------- WEBAPP SERVER SETUP ---------------------------------//
@@ -30,8 +32,8 @@ io.on('connect', function(socket) {
   var questionNum = 0; // keep count of question, used for IF condition.
   socket.on('loaded', function() { // we wait until the client has loaded and contacted us that it is ready to go.
 
-    socket.emit('answer', "Hey, hello I am \"___*-\" a simple chat bot example."); //We start with the introduction;
-    setTimeout(timedQuestion, 5000, socket, "What is your name?"); // Wait a moment and respond with a question.
+    socket.emit('answer', "Hello I am ReallityCheck, I will help you decide if you need to start studying."); //We start with the introduction;
+    setTimeout(timedQuestion, 5000, socket, "How many days do you have left before the test?"); // Wait a moment and respond with a question.
 
   });
   socket.on('message', (data) => { // If we get a new message from the client we process it;
@@ -44,51 +46,73 @@ io.on('connect', function(socket) {
 });
 //--------------------------CHAT BOT FUNCTION-------------------------------//
 function bot(data, socket, questionNum) {
-  var input = data; // This is generally really terrible from a security point of view ToDo avoid code injection
+  var input = data;
   var answer;
   var question;
   var waitTime;
+  var udays;
+  var count;
+  var edays;
+  var temp;
 
-  /// These are the main statments that make up the conversation.
+
   if (questionNum == 0) {
-    answer = 'Hello ' + input + ' :-)'; // output response
+    days = Number(input);
+    answer = 'Ok so you have ' + days + ' days left to study';
     waitTime = 5000;
-    question = 'How old are you?'; // load next question
+    question = 'How long do you usually take to study for a test of this size in days?';
   } else if (questionNum == 1) {
-    answer = 'Really, ' + input + ' years old? So that means you were born in: ' + (2018 - parseInt(input)); // output response
-    waitTime = 5000;
-    question = 'Where do you live?'; // load next question
-  } else if (questionNum == 2) {
-    answer = 'Cool! I have never been to ' + input + '.';
-    waitTime = 5000;
-    question = 'Whats your favorite color?'; // load next question
-  } else if (questionNum == 3) {
-    answer = 'Ok, ' + input + ' it is.';
-    socket.emit('changeBG', input.toLowerCase());
-    waitTime = 5000;
-    question = 'Can you still read the font?'; // load next question
-  } else if (questionNum == 4) {
-    if (input.toLowerCase() === 'yes' || input === 1) {
-      answer = 'Perfect!';
-      waitTime = 5000;
-      question = 'Whats your favorite place?';
-    } else if (input.toLowerCase() === 'no' || input === 0) {
-      socket.emit('changeFont', 'white'); /// we really should look up the inverse of what we said befor.
-      answer = ''
-      question = 'How about now?';
-      waitTime = 0;
-      questionNum--; // Here we go back in the question number this can end up in a loop
-    } else {
-      question = 'Can you still read the font?'; // load next question
-      answer = 'I did not understand you. Could you please answer "yes" or "no"?'
-      questionNum--;
-      waitTime = 5000;
+    udays = Number(input);
+    tdays = days-udays;
+    if (udays <= days){
+      answer = 'Ok so atleast you have ' + tdays + ' days more remaining than you usually take to study.';
     }
-    // load next question
+    if (udays > days){
+      answer = 'Ok so you have ' + tdays + ' days less remaining than you usually take to study.';
+    }
+    waitTime = 5000;
+    question = 'Do you have any other work to do between now and the test?'; // load next question
+  } else if (questionNum == 2) {
+    if (input == 'yes'){
+      answer = 'You relly need to finish that other work as soon as possible.';
+      waitTime = 5000;
+      question = 'How many days will your other work take to complete?';
+    }
+    if (input == 'no'){
+      answer = 'Ok that is good means you only have to focus on the test.';
+      questionNum++;
+      waitTime = 5000;
+      question = 'Do you need to increase your grade in this subject?';
+    }
+  } else if (questionNum == 3) {
+    edays = Number(input);
+    tdays = tdays - edays;
+    answer = 'Ok so you will need to plan for ' + edays + ' days extra to make time for the other work.';
+    waitTime = 5000;
+    question = 'Do you need to increase your grade in this subject?'; // load next question
+  } else if (questionNum == 4) {
+    if (input == 'yes'){
+      answer = 'Ok so you will need longer than usual to study.';
+    }
+    if (input == 'no'){
+      answer = 'Ok that is good and means that your normal study time should be sufficient.';
+    }
+    waitTime = 5000;
+    question = 'How many more days than usual will you need to study to get the higher grade?'; // load next question
+  } else if (questionNum == 5) {
+    edays = Number(input);
+    tdays = tdays - edays;
+    if (tdays >= 0){
+      answer = 'You have ' + tdays + ' more days to study than you need. You can take it easy';
+    }
+    if (tdays < 0){
+      tdays = tdays * -1;
+      answer = 'You need ' + tdays + ' more days to study than you have left. You might to pull a few all nighters';
+    }
+    waitTime = 5000;
   } else {
     answer = 'I have nothing more to say!'; // output response
-    waitTime = 0;
-    question = '';
+    waitTime = 5000;
   }
 
 
